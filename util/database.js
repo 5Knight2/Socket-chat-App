2e099137d16728a253f272bb9f93ef147d5a8945
5Knight2 <pratiktarale258@gmail.com>
2023-07-22 17:19:20 +0530

    Env added

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize=require('sequelize')
 
-const DB=new Sequelize('chat_app','root','Password@123',{
+const DB=new Sequelize(process.env.SQL_PROJECT,process.env.SQL_USER,process.env.SQL_PASSWORD,{
     dialect:'mysql', 
-    host:'localhost'
+    host:process.env.SQL_HOST
 })
 
 module.exports=DB;
